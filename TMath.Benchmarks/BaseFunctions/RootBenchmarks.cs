d1c79ddf15a3b7ea35756f76a054599ed31fd98a
Thiago <thiagomvas@gmail.com>
2023-12-23 16:54:41 -0300

    Add Root Function Benchmarks

---
+++ TMath.Benchmarks/BaseFunctions/RootBenchmarks.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+namespace TMath.Benchmarks.BaseFunctions
+{
+    public class RootBenchmarks
+    {
+        public double Value = 8d;
+        [Benchmark]
+        public double TSqrt() => TFunctions.Sqrt(Value);
+        [Benchmark]
+        public double TSqrt_With_NRoot() => TFunctions.NRoot(Value, 2);
+        [Benchmark]
+        public double MathSqrt() => Math.Sqrt(Value);
+
+        [Benchmark]
+        public double TCbrt() => TFunctions.Cbrt(Value);
+
+        [Benchmark]
+        public double TCbrt_With_NRoot() => TFunctions.NRoot(Value, 3);
+
+        [Benchmark]
+        public double MathCbrt() => Math.Pow(Value, 1d / 3d);
+
+
+
+
+    }
+}
