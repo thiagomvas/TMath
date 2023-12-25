72b231349c386c33e3e416cb7fc8889fcfcbe645
Thiago <thiagomvas@gmail.com>
2023-12-24 21:13:39 -0300

    Add TStatistics module. Add Mean and Median functions

---
+++ TMath/Modules/TStatistics.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+
+namespace TMath.Modules
+{
+    /// <summary>
+    /// A mathematical utility class providing statistical functions for data analysis, with functions like Mean, Standard deviation, and more.
+    /// </summary>
+    /// <remarks>
+    /// This class supports generic calculations on any <see cref="IEnumerable{T}"/> where T is an <see cref="INumber{TSelf}"/>, encompassing
+    /// floats, doubles, decimals, integers, or custom number types that implement the interface.
+    /// </remarks>
+    public static class TStatistics
+    {
+        public static T Mean<T>(IEnumerable<T> data) where T : INumber<T>
+        {
+            T sum = T.Zero;
+            foreach (T d in data)
+                sum += d;
+            return sum / T.CreateSaturating(data.Count());
+        }
+
+        public static T Median<T>(IEnumerable<T> data) where T : INumber<T>
+        {
+            T[] sorted = data.ToArray();
+            Array.Sort(sorted);
+            if(sorted.Length % 2 == 0)
+                return (sorted[sorted.Length / 2] + sorted[sorted.Length / 2 - 1]) / TFunctions.IntToT<T>(2);
+            else return sorted[sorted.Length / 2];
+        }
+    }
+}
