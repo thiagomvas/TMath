85e0c82ee16bbc01fad6adc4de9a2c2ee70ed22c
Thiago <thiagomvas@gmail.com>
2023-12-26 14:36:02 -0300

    Create Root Tests

---
+++ TMath.Tests/Numerics/RootTests.cs
@@ -0,0 +1,86 @@
+﻿namespace TMath.Tests.Numerics
+{
+    public class RootTests
+    {
+        [Test]
+        [TestCase(0d)]
+        [TestCase(1d)]
+        [TestCase(-1d)]
+        [TestCase(2d)]
+        [TestCase(2.5d)]
+        [TestCase(10d)]
+        [TestCase(10000d)]
+
+        public void SqrtTest(double number)
+        {
+            // Arrange
+            var expected = Math.Sqrt(number);
+
+            // Act
+            var actual = TFunctions.Sqrt(number);
+
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual).Within(0.0001));
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(-1)]
+        [TestCase(2)]
+        [TestCase(4)]
+        [TestCase(10)]
+        [TestCase(10000)]
+        public void SqrtTest_NonIRootFunctions(int num)
+        {
+            // Arrange
+            var expected = Math.Sqrt(num);
+
+            // Act
+            var actual = TFunctions.Sqrt<double, int>(num);
+
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual).Within(0.0001));
+        }
+
+        [Test]
+        [TestCase(0d)]
+        [TestCase(1d)]
+        [TestCase(-1d)]
+        [TestCase(2d)]
+        [TestCase(2.5d)]
+        [TestCase(10d)]
+        [TestCase(10000d)]
+        public void CbrtTest(double number)
+        {
+            // Arrange
+            var expected = Math.Cbrt(number);
+
+            // Act
+            var actual = TFunctions.Cbrt(number);
+
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual).Within(0.0001));
+        }
+
+        [Test]
+        [TestCase(0, 4)]
+        [TestCase(1, 4)]
+        [TestCase(-1, 4)]
+        [TestCase(2, 4)]
+        [TestCase(4, 4)]
+        [TestCase(10, 4)]
+        [TestCase(10000, 4)]
+        public void NthRootTest(double number, int n)
+        {
+            // Arrange
+            var expected = Math.Pow(number, 1d / n);
+
+            // Act
+            var actual = TFunctions.NRoot(number, n);
+
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual).Within(0.0001));
+        }
+    }
+}
--- TMath.Tests/Numerics/TStatisticsTests.cs
+++ TMath.Tests/Numerics/TStatisticsTests.cs
@@ -1,18 +1,18 @@
-﻿using TMath.Modules;
+﻿using TMath.Numerics;
 using TMath.Numerics.Models;
 
-namespace TMath.Tests
+namespace TMath.Tests.Numerics
 {
     public class TStatisticsTests
     {
         [Test]
         [TestCase(new double[] { }, 0d)]
-        [TestCase(new[] {1d}, 1d)]
-        [TestCase(new[] {0d, 0d, 0d, 0d, 0d, 0d}, 0d)]
-        [TestCase(new[] {1.94, -5.12, 3.14, 4.20, 2.12}, 1.256d)]
-        [TestCase(new[] {1, 1.1, 0.1, 3, 1, 3, 12, 0.1, 0.2, -3}, 1.85d)]
+        [TestCase(new[] { 1d }, 1d)]
+        [TestCase(new[] { 0d, 0d, 0d, 0d, 0d, 0d }, 0d)]
+        [TestCase(new[] { 1.94, -5.12, 3.14, 4.20, 2.12 }, 1.256d)]
+        [TestCase(new[] { 1, 1.1, 0.1, 3, 1, 3, 12, 0.1, 0.2, -3 }, 1.85d)]
         [TestCase(new[] { -1d, -2, -3, -4, -5 }, -3d)]
-        [TestCase(new[] { 1845d, 894, 923, 18, 261, -974, -98, 651, -654, -874}, 199.2d)]
+        [TestCase(new[] { 1845d, 894, 923, 18, 261, -974, -98, 651, -654, -874 }, 199.2d)]
         public void Mean(double[] data, double expected)
         {
             // Arrange
@@ -64,12 +64,12 @@ namespace TMath.Tests
 
         [Test]
         [TestCase(new int[] { }, 0d)]
-        [TestCase(new int[] { 1 },   1d)]
-        [TestCase(new int[] { 0, 0, 0, 0, 0, 0 },  0d)]
-        [TestCase(new int[] { 1, -5, 3, 4, 2 },  2d)]
-        [TestCase(new int[] { 1, 1, 0, 3, 1, 3, 12, 0, 0, -3 },  1d)]
-        [TestCase(new int[] { -1, -2, -3, -4, -5 },  -3d)]
-        [TestCase(new int[] { 1845, 894, 923, 18, 261, -974, -98, 651, -654, -874 },  139.5d)]
+        [TestCase(new int[] { 1 }, 1d)]
+        [TestCase(new int[] { 0, 0, 0, 0, 0, 0 }, 0d)]
+        [TestCase(new int[] { 1, -5, 3, 4, 2 }, 2d)]
+        [TestCase(new int[] { 1, 1, 0, 3, 1, 3, 12, 0, 0, -3 }, 1d)]
+        [TestCase(new int[] { -1, -2, -3, -4, -5 }, -3d)]
+        [TestCase(new int[] { 1845, 894, 923, 18, 261, -974, -98, 651, -654, -874 }, 139.5d)]
         public void Median_WithIntegers(int[] data, double expected)
         {
             // Arrange
@@ -82,13 +82,13 @@ namespace TMath.Tests
         }
 
         [Test]
-        [TestCase(new int[] { },  0)]
-        [TestCase(new[] { 1 },  1)]
-        [TestCase(new[] { 0, 0, 0, 0, 0, 0 },  0)]
-        [TestCase(new[] { 1, 2, 3 },  1d)]
-        [TestCase(new[] { 4, 3, 3, 4, 5, 7, 2, 1, 7, 3, 4, 9, 4 },  4)]
-        [TestCase(new[] { 1, -3, 2, -6, 2, 1, -3, -3, -4, 0, 0, 1 },  1)]
-        [TestCase(new[] { 1, 2, 1, 2, 1, 2 },  1)]
+        [TestCase(new int[] { }, 0)]
+        [TestCase(new[] { 1 }, 1)]
+        [TestCase(new[] { 0, 0, 0, 0, 0, 0 }, 0)]
+        [TestCase(new[] { 1, 2, 3 }, 1d)]
+        [TestCase(new[] { 4, 3, 3, 4, 5, 7, 2, 1, 7, 3, 4, 9, 4 }, 4)]
+        [TestCase(new[] { 1, -3, 2, -6, 2, 1, -3, -3, -4, 0, 0, 1 }, 1)]
+        [TestCase(new[] { 1, 2, 1, 2, 1, 2 }, 1)]
         public void Mode(int[] data, double expected)
         {
             // Arrange
@@ -102,13 +102,13 @@ namespace TMath.Tests
 
 
         [Test]
-        [TestCase(new double[] { },  0d)]
-        [TestCase(new[] { 1d },  0d)]
-        [TestCase(new[] { 0d, 0d, 0d, 0d, 0d, 0d },  0d)]
-        [TestCase(new[] { 1d, 2, 3 },  1d)]
-        [TestCase(new[] { 1, 1.1, 0.1, 3, 0.9, -0.1 },  1.208d)]
-        [TestCase(new[] { -1d, -2, -3, -4, -5 },  2.5d)]
-        [TestCase(new[] { 237d, 589, 412, 765, 321 },  45226.2d)]
+        [TestCase(new double[] { }, 0d)]
+        [TestCase(new[] { 1d }, 0d)]
+        [TestCase(new[] { 0d, 0d, 0d, 0d, 0d, 0d }, 0d)]
+        [TestCase(new[] { 1d, 2, 3 }, 1d)]
+        [TestCase(new[] { 1, 1.1, 0.1, 3, 0.9, -0.1 }, 1.208d)]
+        [TestCase(new[] { -1d, -2, -3, -4, -5 }, 2.5d)]
+        [TestCase(new[] { 237d, 589, 412, 765, 321 }, 45226.2d)]
 
         public void Variance(double[] data, double expected)
         {
@@ -122,13 +122,13 @@ namespace TMath.Tests
         }
 
         [Test]
-        [TestCase(new int[] { },  0d)]
-        [TestCase(new int[] { 1 },  0d)]
-        [TestCase(new int[] { 0, 0, 0, 0, 0, 0 },  0d)]
-        [TestCase(new int[] { 1, 2, 3 },  1d)]
-        [TestCase(new int[] { 1, 1, 0, 3, 1, 3, 12, 0, 0, -5 },  274 / 15d)]
-        [TestCase(new int[] { -1, -2, -3, -4, -5 },  2.5d)]
-        [TestCase(new int[] { 237, 589, 412, 765, 321 },  45226.2d)]
+        [TestCase(new int[] { }, 0d)]
+        [TestCase(new int[] { 1 }, 0d)]
+        [TestCase(new int[] { 0, 0, 0, 0, 0, 0 }, 0d)]
+        [TestCase(new int[] { 1, 2, 3 }, 1d)]
+        [TestCase(new int[] { 1, 1, 0, 3, 1, 3, 12, 0, 0, -5 }, 274 / 15d)]
+        [TestCase(new int[] { -1, -2, -3, -4, -5 }, 2.5d)]
+        [TestCase(new int[] { 237, 589, 412, 765, 321 }, 45226.2d)]
         public void Variance_WithIntegers(int[] data, double expected)
         {
             // Arrange
@@ -141,13 +141,13 @@ namespace TMath.Tests
         }
 
         [Test]
-        [TestCase(new double[] { },  0d)]
-        [TestCase(new[] { 1d },  0d)]
-        [TestCase(new[] { 0d, 0d, 0d, 0d, 0d, 0d },  0d)]
-        [TestCase(new[] { 1d, 2, 3 },  1d)]
-        [TestCase(new[] { -1d, -2, -3 },  1d)]
-        [TestCase(new[] { -1d, -2, -3, -4, -5 },  1.5811d)]
-        [TestCase(new[] { 237d, 589, 412, 765, 321 },  212.6645d)]
+        [TestCase(new double[] { }, 0d)]
+        [TestCase(new[] { 1d }, 0d)]
+        [TestCase(new[] { 0d, 0d, 0d, 0d, 0d, 0d }, 0d)]
+        [TestCase(new[] { 1d, 2, 3 }, 1d)]
+        [TestCase(new[] { -1d, -2, -3 }, 1d)]
+        [TestCase(new[] { -1d, -2, -3, -4, -5 }, 1.5811d)]
+        [TestCase(new[] { 237d, 589, 412, 765, 321 }, 212.6645d)]
         public void StandardDeviation(double[] data, double expected)
         {
             // Arrange
@@ -160,12 +160,12 @@ namespace TMath.Tests
         }
 
         [Test]
-        [TestCase(new int[] { },  0d)]
-        [TestCase(new int[] { 1 },  0d)]
-        [TestCase(new int[] { 0, 0, 0, 0, 0, 0 },  0d)]
-        [TestCase(new int[] { 1, 2, 3 },  1d)]
-        [TestCase(new int[] { -1, -2, -3, -4, -5 },  1.5811d)]
-        [TestCase(new int[] { 237, 589, 412, 765, 321 },  212.6645d)]
+        [TestCase(new int[] { }, 0d)]
+        [TestCase(new int[] { 1 }, 0d)]
+        [TestCase(new int[] { 0, 0, 0, 0, 0, 0 }, 0d)]
+        [TestCase(new int[] { 1, 2, 3 }, 1d)]
+        [TestCase(new int[] { -1, -2, -3, -4, -5 }, 1.5811d)]
+        [TestCase(new int[] { 237, 589, 412, 765, 321 }, 212.6645d)]
         public void StandardDeviation_WithIntegers(int[] data, double expected)
         {
             // Arrange
@@ -178,21 +178,21 @@ namespace TMath.Tests
         }
 
         [Test]
-        [TestCase(new double[] { },  95d, 0d)]
-        [TestCase(new[] { 1d },  50d, 1d)]
-        [TestCase(new[] { 0d, 0d, 0d, 0d, 0d, 0d },  50d, 0d)]
-        [TestCase(new[] { 1d, 2, 3 },  50d, 2d)]
-        [TestCase(new[] { 1d, 2, 3 },  95d, 3d)]
-        [TestCase(new[] { -1d, -2, -3 },  30d, -3d)]
-        [TestCase(new[] { -1d, -2, -3 },  95d, -1d)]
-        [TestCase(new[] { -1d, -2, -3, -4 },  50d, -2.5d)]
+        [TestCase(new double[] { }, 95d, 0d)]
+        [TestCase(new[] { 1d }, 50d, 1d)]
+        [TestCase(new[] { 0d, 0d, 0d, 0d, 0d, 0d }, 50d, 0d)]
+        [TestCase(new[] { 1d, 2, 3 }, 50d, 2d)]
+        [TestCase(new[] { 1d, 2, 3 }, 95d, 3d)]
+        [TestCase(new[] { -1d, -2, -3 }, 30d, -3d)]
+        [TestCase(new[] { -1d, -2, -3 }, 95d, -1d)]
+        [TestCase(new[] { -1d, -2, -3, -4 }, 50d, -2.5d)]
         public void Percentile(double[] data, double percentile, double expected)
         {
             // Arrange
 
             // Act
             var actual = TStatistics.Percentile(data, percentile);
-            
+
             // Assert
             Assert.That(expected, Is.EqualTo(actual).Within(0.0001));
         }
