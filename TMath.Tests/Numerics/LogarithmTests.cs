4835d6b53af403e264108ddcee5808119fefdaf0
Thiago <thiagomvas@gmail.com>
2023-12-26 14:53:41 -0300

    Add Logarithm Tests

---
+++ TMath.Tests/Numerics/LogarithmTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMath.Tests.Numerics
+{
+    public class LogarithmTests
+    {
+        [Test]
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(-1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(1000)]
+
+        public void Log2Test(double num)
+        {
+            // Arrange
+            double expected = Math.Log(num, 2);
+
+            // Act
+            double actual = TFunctions.Log2(num);
+
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual).Within(0.0001));
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(-1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(1000)]
+        public void Log10Test(double num)
+        {
+            // Arrange
+            double expected = Math.Log10(num);
+
+            // Act
+            double actual = TFunctions.Log10(num);
+
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual).Within(0.0001));
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(-1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(1000)]
+        public void LnTest(double num)
+        {
+            // Arrange
+            double expected = Math.Log(num);
+
+            // Act
+            double actual = TFunctions.Ln(num);
+
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        [TestCase(0, 2)]
+        [TestCase(1, 2)]
+        [TestCase(-1, 2)]
+        [TestCase(2, 2)]
+        [TestCase(3, 2)]
+        [TestCase(4, 2)]
+        [TestCase(1000, 2)]
+        public void LogNTest(double num, double @base)
+        {
+            // Arrange
+            double expected = Math.Log(num, @base);
+
+            // Act
+            double actual = TFunctions.Log(num, @base);
+
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual).Within(0.0001));
+        }
+    }
+}
