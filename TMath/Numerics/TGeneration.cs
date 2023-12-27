a9406692825a0b2b240d15af6055f8e4500c4af4
Thiago <thiagomvas@gmail.com>
2023-12-27 17:15:50 -0300

    Add RandomNumber function

--- TMath/Numerics/TGeneration.cs
+++ TMath/Numerics/TGeneration.cs
@@ -16,5 +16,19 @@ namespace TMath.Numerics
             random = new Random(seed);
         }
 
+        public T RandomNumber<T>(T min, T max) where T : INumber<T>
+        {
+            // If min is bigger than max, swap them
+            if (min > max)
+            {
+                (min, max) = (max, min);
+            }
+
+            T range = max - min;
+            double randomValue = random.NextDouble();
+
+            return min + range * T.CreateSaturating(randomValue);
+        }
+
     }
 }
