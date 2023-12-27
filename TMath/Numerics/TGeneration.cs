90ec4d8675f97357be923849a052c1ce4d51f68c
Thiago <thiagomvas@gmail.com>
2023-12-27 17:19:58 -0300

    Add RandomArray

--- TMath/Numerics/TGeneration.cs
+++ TMath/Numerics/TGeneration.cs
@@ -16,6 +16,8 @@ namespace TMath.Numerics
             random = new Random(seed);
         }
 
+        // FOR INTS: min is inclusive, max is exclusive
+        // FOR FLOATS: min and max are inclusive
         public T RandomNumber<T>(T min, T max) where T : INumber<T>
         {
             // If min is bigger than max, swap them
@@ -29,6 +31,17 @@ namespace TMath.Numerics
 
             return min + range * T.CreateSaturating(randomValue);
         }
+
+        public T[] RandomArray<T>(int length, T min, T max) where T : INumber<T>
+        {
+            T[] result = new T[length];
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = RandomNumber(min, max);
+            }
+            return result;
+        }
+
 
     }
 }
