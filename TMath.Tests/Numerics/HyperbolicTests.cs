5c875780a7848734ccc3f63fb7bf9126790d02ab
Thiago <thiagomvas@gmail.com>
2023-12-26 21:49:12 -0300

    Clean up for merge

--- TMath.Tests/Numerics/HyperbolicTests.cs
+++ TMath.Tests/Numerics/HyperbolicTests.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TMath.Tests.Numerics
+﻿namespace TMath.Tests.Numerics
 {
     public class HyperbolicTests
     {
