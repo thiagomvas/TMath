0a6aabc4fa8d8ac2c8873203091fb728dc471c11
Thiago <thiagomvas@gmail.com>
2023-12-27 17:11:38 -0300

    Create TGeneration class

---
+++ TMath/Numerics/TGeneration.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace TMath.Numerics
+{
+    public class TGeneration
+    {
+        private readonly Random random;
+        public static readonly TGeneration Default = new TGeneration(0);
+        public TGeneration()
+        {
+            random = new Random();
+        }
+
+        public TGeneration(int seed)
+        {
+            random = new Random(seed);
+        }
+
+    }
+}
