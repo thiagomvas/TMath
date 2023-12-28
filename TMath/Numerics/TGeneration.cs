9bc67ee1fdf7c44904b2bba80f6deb7d024f4f85
Thiago <thiagomvas@gmail.com>
2023-12-27 21:51:54 -0300

    Add XML Doc Comments

--- TMath/Numerics/TGeneration.cs
+++ TMath/Numerics/TGeneration.cs
@@ -2,9 +2,20 @@
 
 namespace TMath.Numerics
 {
+    /// <summary>
+    /// A utility class for generating random numbers and sequences of numbers.
+    /// </summary>
+    /// <remarks>
+    /// It uses a seed for the random number generator, so if you want to generate the same numbers every time, use the same seed.
+    /// Otherwise, use <see cref="TGeneration.Default"/> for the default instance of this class that always has the same seed.
+    /// </remarks>
     public class TGeneration
     {
         private readonly Random random;
+
+        /// <summary>
+        /// The default instance of <see cref="TGeneration"/> that always has the same seed.
+        /// </summary>
         public static readonly TGeneration Default = new TGeneration(0);
         public TGeneration()
         {
@@ -16,8 +27,17 @@ namespace TMath.Numerics
             random = new Random(seed);
         }
 
-        // FOR INTS: min is inclusive, max is exclusive
-        // FOR FLOATS: min and max are inclusive
+        /// <summary>
+        /// Generates a random number between <paramref name="min"/> and <paramref name="max"/>.
+        /// </summary>
+        /// <typeparam name="T">The numeric type implementing the INumber interface.</typeparam>
+        /// <param name="min">The minimum value of the range.</param>
+        /// <param name="max">The maximum value of the range.</param>
+        /// <returns>A random number within the specified range.</returns>
+        /// <remarks>
+        /// For floating-point types, both the minimum and maximum values are inclusive. <br/>
+        /// For integer types, the minimum value is inclusive, and the <strong>maximum value is exclusive.</strong>
+        /// </remarks>
         public T RandomNumber<T>(T min, T max) where T : INumber<T>
         {
             // If min is bigger than max, swap them
@@ -32,6 +52,14 @@ namespace TMath.Numerics
             return min + range * T.CreateSaturating(randomValue);
         }
 
+        /// <summary>
+        /// Generates a random array of numbers within the specified range.
+        /// </summary>
+        /// <typeparam name="T">The numeric type implementing the INumber interface.</typeparam>
+        /// <param name="length">The length of the array to generate.</param>
+        /// <param name="min">The minimum value of the array elements.</param>
+        /// <param name="max">The maximum value of the array elements.</param>
+        /// <returns>An array of random numbers within the specified range.</returns>
         public T[] RandomArray<T>(int length, T min, T max) where T : INumber<T>
         {
             T[] result = new T[length];
@@ -42,6 +70,13 @@ namespace TMath.Numerics
             return result;
         }
 
+        /// <summary>
+        /// Generates an array where each element is a sequence of results from the specified function, starting from 0.
+        /// </summary>
+        /// <typeparam name="T">The numeric type implementing the INumber interface.</typeparam>
+        /// <param name="function">The function to apply to generate the sequence.</param>
+        /// <param name="length">The length of the resulting array.</param>
+        /// <returns>An array representing the sequence of results from the specified function.</returns>
         public T[] FunctionSequence<T>(Func<T,T> function, T length) where T : INumber<T>
         {
             T[] result = new T[int.CreateSaturating(length)];
@@ -54,9 +89,16 @@ namespace TMath.Numerics
             return result;
         }
 
+        /// <summary>
+        /// Generates an array where each element is a sequence of results from the specified function.
+        /// </summary>
+        /// <typeparam name="T">The numeric type implementing the INumber interface.</typeparam>
+        /// <param name="function">The function to apply to generate the sequence.</param>
+        /// <param name="start">The starting point of the sequence.</param>
+        /// <param name="end">The ending point of the sequence.</param>
+        /// <returns>An array representing the sequence of results from the specified function.</returns>
         public T[] FunctionSequence<T>(Func<T, T> function, T start, T end) where T : INumber<T>
         {
-
             // If min is bigger than max, swap them
             if (start > end)
             {
@@ -74,6 +116,15 @@ namespace TMath.Numerics
             return result;
         }
 
+        /// <summary>
+        /// Generates an array where each element is a sequence of results from the specified function.
+        /// </summary>
+        /// <typeparam name="T">The numeric type implementing the INumber interface.</typeparam>
+        /// <param name="function">The function to apply to generate the sequence.</param>
+        /// <param name="start">The starting point of the sequence.</param>
+        /// <param name="end">The ending point of the sequence.</param>
+        /// <param name="step">The step size between elements in the sequence.</param>
+        /// <returns>An array representing the sequence of results from the specified function.</returns>
         public T[] FunctionSequence<T>(Func<T, T> function, T start, T end, T step) where T : INumber<T>
         {
             if (start == end && start == T.Zero) return new T[0];
