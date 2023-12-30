88fdd616fa864f11f8ecf196f14895b84133e1e7
Thiago <thiagomvas@gmail.com>
2023-12-30 12:09:57 -0300

    Add Test Console Project

---
+++ TMath.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using TMath.Numerics;
+namespace TMath.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = TGeneration.Default.RandomArray<int>(1000, 0, 10);
+            foreach (var item in data)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
