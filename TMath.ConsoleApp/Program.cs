d5876a1ac22334f2ffa94faf40e7482659e01f1a
Thiago <thiagomvas@gmail.com>
2024-01-13 10:18:24 -0300

    Add LUDecomposition

--- TMath.ConsoleApp/Program.cs
+++ TMath.ConsoleApp/Program.cs
@@ -1,14 +1,40 @@
-﻿using TMath.Numerics;
-using TMath.Numerics.Models;
+﻿using TMath.Numerics.AdvancedMath;
 namespace TMath.ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var data = TGeneration.Default.RandomArray<int>(1000, 0, 1);
-            DescriptiveStatistics<int> stats = new(data);
-            Console.WriteLine(stats);
+            double[][] matrix = 
+            {
+                [0, 2, 3, 4],
+                [5, 6, 7, 8],
+                [9, 1, 2, 3],
+                [4, 5, 6, 7]
+            };
+
+            TLinearAlgebra.LUDecomposition(matrix, out double[][] L, out double[][] U);
+
+            Console.WriteLine("Matrix: ");
+            Print2DArray(matrix);
+            Console.WriteLine("");
+            Console.WriteLine("L: ");
+            Print2DArray(L);
+            Console.WriteLine("");
+            Console.WriteLine("U: ");
+            Print2DArray(U);
+        }
+
+        public static void Print2DArray<T>(T[][] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    Console.Write(matrix[i][j] + "\t");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
--- TMath/Numerics/AdvancedMath/TLinearAlgebra.cs
+++ TMath/Numerics/AdvancedMath/TLinearAlgebra.cs
@@ -1,6 +1,74 @@
-﻿namespace TMath.Numerics.AdvancedMath
+﻿using System.Numerics;
+
+namespace TMath.Numerics.AdvancedMath
 {
-    public class TLinearAlgebra
+    public static class TLinearAlgebra
     {
+        private static bool checkIfSolved<T>(T[][] matrix) where T : INumber<T>
+        {
+            int n = matrix.Length;
+            bool solved = true;
+
+            for (int y = 1; y < n; y++)
+            {
+                for (int x = 0; x < y; x++)
+                {
+                    if (!matrix[y][x].Equals(T.Zero))
+                    {
+                        solved = false;
+                        break;
+                    }
+                }
+            }
+
+            return solved;
+        }
+
+        // TO-DO: Fix for pivot = 0;
+        public static void LUDecomposition<T>(T[][] matrix, out T[][] L, out T[][] U)
+            where T : INumber<T>
+        {
+            if (matrix.Length != matrix[0].Length)
+                throw new ArgumentException("Matrix must be square", nameof(matrix));
+
+            int n = matrix.Length;
+            L = new T[n][];
+            U = new T[n][];
+            T[][] P = new T[n][];
+            for (int i = 0; i < n; i++)
+            {
+                L[i] = new T[n];
+                U[i] = new T[n];
+                P[i] = new T[n];
+                L[i][i] = T.One;
+                P[i][i] = T.One;
+
+                for (int j = 0; j < n; j++)
+                {
+                    U[i][j] = matrix[i][j];
+                }
+            }
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (checkIfSolved(U))
+                    break;
+                if (U[i][i].Equals(T.Zero))
+                {
+                    (U[i], U[i + 1]) = (U[i + 1], U[i]);
+                }    
+
+                for (int row = i + 1; row < n; row++)
+                {
+                    T coef = -U[row][i] / U[i][i];
+                    L[row][i] = -coef;
+
+                    for (int column = i; column < n; column++)
+                    {
+                        U[row][column] = TFunctions.Round(U[row][column] + coef * U[i][column], 10);
+                    }
+                }
+            }
+        }
     }
 }
