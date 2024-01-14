93419b7625569e938fdb3ba6bb18458838a25174
Thiago <thiagomvas@gmail.com>
2024-01-14 16:21:02 -0300

    Add Vector interface

---
+++ TMath/Numerics/Interfaces/ITVector.cs
@@ -0,0 +1,10 @@
+﻿using TMath.Numerics.LinearAlgebra;
+
+namespace TMath.Numerics.Interfaces
+{
+	public interface ITVector<T>
+	{
+		public ITVector<T> One { get; }
+		public ITVector<T> Zero { get; }
+	}
+}
