429c8d155621d99c0b2257d9fb704096d9efa22e
Thiago <thiagomvas@gmail.com>
2024-01-14 16:21:17 -0300

    Add base TVector class with no implementations

---
+++ TMath/Numerics/LinearAlgebra/TVector.cs
@@ -0,0 +1,83 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
+
+namespace TMath.Numerics.LinearAlgebra
+{
+	public class TVector<T>
+		: IAdditionOperators<TVector<T>, TVector<T>, TVector<T>>,
+		ISubtractionOperators<TVector<T>, TVector<T>, TVector<T>>,
+		IMultiplyOperators<TVector<T>, T, TVector<T>>,
+		IDivisionOperators<TVector<T>, T, TVector<T>>,
+		IEqualityOperators<TVector<T>, TVector<T>, bool>,
+		IComparisonOperators<TVector<T>, TVector<T>, bool>,
+		IIncrementOperators<TVector<T>>,
+		IDecrementOperators<TVector<T>>,
+		IAdditiveIdentity<TVector<T>, TVector<T>>,
+		IMultiplicativeIdentity<TVector<T>, T>
+		where T : INumber<T>, new()
+	{
+		public static TVector<T> AdditiveIdentity => throw new NotImplementedException();
+
+		public static T MultiplicativeIdentity => throw new NotImplementedException();
+
+		public static TVector<T> operator +(TVector<T> left, TVector<T> right)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static TVector<T> operator -(TVector<T> left, TVector<T> right)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static TVector<T> operator ++(TVector<T> value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static TVector<T> operator --(TVector<T> value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static bool operator ==(TVector<T>? left, TVector<T>? right)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static bool operator !=(TVector<T>? left, TVector<T>? right)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static bool operator <(TVector<T> left, TVector<T> right)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static bool operator >(TVector<T> left, TVector<T> right)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static bool operator <=(TVector<T> left, TVector<T> right)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static bool operator >=(TVector<T> left, TVector<T> right)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static TVector<T> operator /(TVector<T> left, T right)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static TVector<T> operator *(TVector<T> left, T right)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
