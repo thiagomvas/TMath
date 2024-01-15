2d908062e2a9b8f7be4ca591da1c0246fb471a31
Thiago <thiagomvas@gmail.com>
2024-01-15 09:26:19 -0300

    Add base Vector2 and 3 classes

---
+++ TMath/Numerics/LinearAlgebra/TVector2.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Numerics;
+using TMath.Numerics.Interfaces;
+
+namespace TMath.Numerics.LinearAlgebra
+{
+	public class TVector2<T>
+		: IAdditionOperators<TVector2<T>, TVector2<T>, TVector2<T>>,
+		ISubtractionOperators<TVector2<T>, TVector2<T>, TVector2<T>>,
+		IMultiplyOperators<TVector2<T>, T, TVector2<T>>,
+		IDivisionOperators<TVector2<T>, T, TVector2<T>>,
+		IEqualityOperators<TVector2<T>, TVector2<T>, bool>,
+		IComparisonOperators<TVector2<T>, TVector2<T>, bool>,
+		IIncrementOperators<TVector2<T>>,
+		IDecrementOperators<TVector2<T>>,
+		IAdditiveIdentity<TVector2<T>, TVector2<T>>,
+		IMultiplicativeIdentity<TVector2<T>, T>
+		where T : INumber<T>, new()
+	{
+
+		public T X;
+		public T Y;
+
+		public static TVector2<T> AdditiveIdentity => throw new NotImplementedException();
+
+		public static T MultiplicativeIdentity => throw new NotImplementedException();
+
+
+		public static TVector2<T> operator +(TVector2<T> left, TVector2<T> right)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static TVector2<T> operator -(TVector2<T> left, TVector2<T> right)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static TVector2<T> operator ++(TVector2<T> value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static TVector2<T> operator --(TVector2<T> value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static TVector2<T> operator *(TVector2<T> left, T right)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static TVector2<T> operator /(TVector2<T> left, T right)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static bool operator ==(TVector2<T>? left, TVector2<T>? right)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static bool operator !=(TVector2<T>? left, TVector2<T>? right)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static bool operator <(TVector2<T> left, TVector2<T> right)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static bool operator >(TVector2<T> left, TVector2<T> right)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static bool operator <=(TVector2<T> left, TVector2<T> right)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static bool operator >=(TVector2<T> left, TVector2<T> right)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ TMath/Numerics/LinearAlgebra/TVector3.cs
@@ -0,0 +1,89 @@
+﻿using System.Numerics;
+using TMath.Numerics.Interfaces;
+
+namespace TMath.Numerics.LinearAlgebra
+{
+	public class TVector3<T>
+		: IAdditionOperators<TVector3<T>, TVector3<T>, TVector3<T>>,
+		ISubtractionOperators<TVector3<T>, TVector3<T>, TVector3<T>>,
+		IMultiplyOperators<TVector3<T>, T, TVector3<T>>,
+		IDivisionOperators<TVector3<T>, T, TVector3<T>>,
+		IEqualityOperators<TVector3<T>, TVector3<T>, bool>,
+		IComparisonOperators<TVector3<T>, TVector3<T>, bool>,
+		IIncrementOperators<TVector3<T>>,
+		IDecrementOperators<TVector3<T>>,
+		IAdditiveIdentity<TVector3<T>, TVector3<T>>,
+		IMultiplicativeIdentity<TVector3<T>, T>
+		where T : INumber<T>, new()
+	{
+
+		public T X;
+		public T Y;
+		public T Z;
+		
+		public static TVector3<T> AdditiveIdentity => throw new NotImplementedException();
+
+		public static T MultiplicativeIdentity => throw new NotImplementedException();
+
+
+		public static TVector3<T> operator +(TVector3<T> left, TVector3<T> right)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static TVector3<T> operator -(TVector3<T> left, TVector3<T> right)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static TVector3<T> operator ++(TVector3<T> value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static TVector3<T> operator --(TVector3<T> value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static TVector3<T> operator *(TVector3<T> left, T right)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static TVector3<T> operator /(TVector3<T> left, T right)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static bool operator ==(TVector3<T> left, TVector3<T> right)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static bool operator !=(TVector3<T> left, TVector3<T> right)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static bool operator <(TVector3<T> left, TVector3<T> right)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static bool operator >(TVector3<T> left, TVector3<T> right)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static bool operator <=(TVector3<T> left, TVector3<T> right)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static bool operator >=(TVector3<T> left, TVector3<T> right)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
