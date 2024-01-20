79a48e7dd873aad6023aee8264009f762fef04c3
Thiago <thiagomvas@gmail.com>
2024-01-20 16:53:42 -0300

    Add Determinant

--- TMath.ConsoleApp/Program.cs
+++ TMath.ConsoleApp/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Security.Cryptography.X509Certificates;
 using TMath.Numerics.AdvancedMath;
 using TMath.Numerics.LinearAlgebra;
 namespace TMath.ConsoleApp
@@ -7,18 +7,79 @@ namespace TMath.ConsoleApp
     {
         static void Main(string[] args)
         {
-            TMatrix<double> matrix = new(4, 4, [0, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6]);
+            TMatrix<int> matrix = new(4, 4, [2, 6, 51, 61, 65, 1, 98, 65, 19, 81, 63, 5, 49, 1, 6, 15]);
 
             TLinearAlgebra.LUDecomposition(matrix, out var u, out var l, out var p);
 
+            Console.WriteLine("========= MATRIX M =========");
+            Console.WriteLine(matrix);
             Console.WriteLine("========= MATRIX U =========");
             Console.WriteLine(u);
             Console.WriteLine("========= MATRIX L =========");
             Console.WriteLine(l);
             Console.WriteLine("========= MATRIX P =========");
             Console.WriteLine(p);
+            Console.WriteLine("========= MATRIX P * L * U = M =========");
+            Console.WriteLine(p * l * u);
+			Console.WriteLine("========= DETERMINANT =========");
+            Console.WriteLine(TLinearAlgebra.Determinant(matrix));
 
+		}
+
+    static int CalculateDeterminant(int[,] matrix)
+    {
+        int size = matrix.GetLength(0);
+
+        if (size != matrix.GetLength(1))
+        {
+            throw new ArgumentException("Matrix must be square.");
         }
 
+        if (size == 1)
+        {
+            return matrix[0, 0];
+        }
+
+        int result = 0;
+
+        for (int col = 0; col < size; col++)
+        {
+            result += matrix[0, col] * GetSign(0, col) * CalculateDeterminant(GetSubmatrix(matrix, 0, col));
+        }
+
+        return result;
     }
+
+    static int[,] GetSubmatrix(int[,] matrix, int rowToRemove, int colToRemove)
+    {
+        int size = matrix.GetLength(0);
+        int[,] submatrix = new int[size - 1, size - 1];
+
+        int rowIndex = 0;
+        for (int i = 0; i < size; i++)
+        {
+            if (i == rowToRemove)
+                continue;
+
+            int colIndex = 0;
+            for (int j = 0; j < size; j++)
+            {
+                if (j == colToRemove)
+                    continue;
+
+                submatrix[rowIndex, colIndex] = matrix[i, j];
+                colIndex++;
+            }
+
+            rowIndex++;
+        }
+
+        return submatrix;
+    }
+
+    static int GetSign(int row, int col)
+    {
+        return (row + col) % 2 == 0 ? 1 : -1;
+    }
+	}
 }
--- TMath/Numerics/AdvancedMath/TLinearAlgebra.cs
+++ TMath/Numerics/AdvancedMath/TLinearAlgebra.cs
@@ -6,111 +6,25 @@ namespace TMath.Numerics.AdvancedMath
 {
     public static class TLinearAlgebra
 	{
-		public static void LUDecomposition<T>(TMatrix<T> matrix, out TMatrix<T> L, out TMatrix<T> U)
-			where T : INumber<T>
+		public static T Determinant<T>(TMatrix<T> matrix) where T : INumber<T>
 		{
+			int size = matrix.Rows;
 			if (matrix.Rows != matrix.Columns)
 				throw new ArgumentException("Matrix must be square", nameof(matrix));
-
-			int n = matrix.Rows;
-			L = new TMatrix<T>(n, n);
-			U = new TMatrix<T>(n, n);
-			var P = new TMatrix<T>(n, n);
-			for (int i = 0; i < n; i++)
+			if(size == 1)
 			{
-                L[i, i] = T.One;
-				P[i,i] = T.One;
-
-				for (int j = 0; j < n; j++)
-				{
-					U[i,j] = matrix[i,j];
-				}
+				return matrix[0, 0];
 			}
 
-			for (int i = 0; i < n - 1; i++)
+			T result = T.Zero;
+
+			for(int col = 0; col < size; col++)
 			{
-				if (TLinearAlgebraSolvers.CheckIfSolved(U))
-					break;
-
-				if (U[i,i] == T.Zero)
-				{
-					U = U.SwapRows(i, i + 1);
-				}
-
-				for (int row = i + 1; row < n; row++)
-				{
-					T coef = -U[row, i] / U[i, i];
-					L[row, i] = -coef;
-
-					for (int column = i; column < n; column++)
-					{
-						U[row, column] = coef != T.Zero ? U[row, column] + coef * U[i, column] : T.Zero;
-					}
-				}
+				T sign = (col % 2 == 0) ? T.One : -T.One;
+				result += matrix[0, col] * sign * Determinant(matrix.Submatrix(0, col));
 			}
+			return result; ; 
 		}
-
-
-		public static void LUDecomposition<T>(T[][] matrix, out T[][] L, out T[][] U)
-            where T : INumber<T>
-        {
-            if (matrix.Length != matrix[0].Length)
-                throw new ArgumentException("Matrix must be square", nameof(matrix));
-
-            int n = matrix.Length;
-            L = new T[n][];
-            U = new T[n][];
-            T[][] P = new T[n][];
-            for (int i = 0; i < n; i++)
-            {
-                L[i] = new T[n];
-                U[i] = new T[n];
-                P[i] = new T[n];
-                L[i][i] = T.One;
-                P[i][i] = T.One;
-
-                for (int j = 0; j < n; j++)
-                {
-                    U[i][j] = matrix[i][j];
-                }
-            }
-
-            for (int i = 0; i < n - 1; i++)
-            {
-                if (TLinearAlgebraSolvers.checkIfSolved(U))
-                    break;
-                if (U[i][i].Equals(T.Zero))
-                {
-                    (U[i], U[i + 1]) = (U[i + 1], U[i]);
-					Console.WriteLine("Swap");
-                }    
-
-                for (int row = i + 1; row < n; row++)
-                {
-                    T coef = -U[row][i] / U[i][i];
-                    L[row][i] = -coef;
-
-                    for (int column = i; column < n; column++)
-                    {
-                        U[row][column] = coef != T.Zero ? U[row][column] + coef * U[i][column] : T.Zero;
-                    }
-                }
-            }
-        }
-        public static void PrintMatrix<T>(T[][] matrix, string? format = null)
-        {
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                for (int j = 0; j < matrix[i].Length; j++)
-                {
-                    if(i == j) Console.ForegroundColor = ConsoleColor.Green;
-                    string formatted = string.Format($"{{0:{format}}}", matrix[i][j]) ?? matrix[i][j].ToString();
-                    Console.Write($"{formatted}\t");
-                    if (i == j) Console.ResetColor();
-                }
-                Console.WriteLine();
-            }
-        }
 
 		public static void LUDecomposition<T>(TMatrix<T> matrix, out TMatrix<T> U, out TMatrix<T> L, out TMatrix<T> P) where T : INumber<T>
 		{
@@ -140,12 +54,12 @@ namespace TMath.Numerics.AdvancedMath
 				{
 					U = U.SwapRows(pivot, pivot + 1);
 					P = P.SwapRows(pivot, pivot + 1);
-					Console.WriteLine("Swap");
 				}
 
 				for(int row = pivot + 1; row < U.Rows; row++)
 				{
 					T coef = U[row, pivot] / U[pivot, pivot];
+					L[row, pivot] = coef;
 					for(int col = 0; col < U.Columns; col++)
 					{
 						U[row, col] = U[row, col] - coef * U[pivot, col];
