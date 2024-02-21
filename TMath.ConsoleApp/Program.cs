b8a19ee9b5f558f493c9294b80055e321b0ac952
Thiago <thiagomvas@gmail.com>
2024-02-21 13:38:15 -0300

    Add GCD and LCM

--- TMath.ConsoleApp/Program.cs
+++ TMath.ConsoleApp/Program.cs
@@ -1,52 +1,15 @@
+using TMath.Numerics;
 using TMath.Numerics.AdvancedMath.LinearAlgebra;
+using TMath.Numerics.AdvancedMath.NumberTheory;
 namespace TMath.ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            TMatrix<double> matrix = new(3, 5, [1, 2, -8, 1, -3, 4, -3, 12, 37, -5, 2, 1, 24, -17, 8]);
-
-            TLinearAlgebra.LUDecomposition(matrix, out var u, out var l, out var p, out var steps);
-
-
-            Console.WriteLine("STEP BY STEP ");
-
-            foreach (var s in steps)
-            {
-                Console.WriteLine("=========================");
-                Console.WriteLine(s.CurrentState);
-                Console.WriteLine(s.NextOperation);
-            }
-            Console.WriteLine(u);
-            Console.WriteLine("=========================");
-
-            //Console.Write("\n\n\n");
-            Console.WriteLine("========= MATRIX M =========");
-            Console.WriteLine(matrix);
-            //Console.WriteLine("========= DETERMINANT =========");
-            //Console.WriteLine(TLinearAlgebra.Determinant(matrix));
-            //Console.WriteLine("========= RANK =========");
-            //Console.WriteLine(TLinearAlgebra.Rank(matrix));
-            Console.WriteLine("========= MATRIX U =========");
-            Console.WriteLine(u);
-            Console.WriteLine("========= MATRIX L =========");
-            Console.WriteLine(l);
-            Console.WriteLine("========= MATRIX P =========");
-            Console.WriteLine(p);
-            //Console.WriteLine((p * l * u));
-            //Console.WriteLine("========= MATRIX P * L * U = M =========");
-            //Console.WriteLine("========= INVERSE =========");
-            //Console.WriteLine(TLinearAlgebra.Inverse(matrix));
-            //Console.WriteLine("========= ROW ECHELON =========");
-            //Console.WriteLine(TLinearAlgebra.RowEchelon(matrix));
-            //Console.WriteLine("========= MATRIX A * A^-1 =========");
-            //Console.WriteLine((matrix * TLinearAlgebra.Inverse(matrix)).RoundValues(5));
-            //Console.WriteLine("========= MATRIX IN LATEX =========");
-            //Console.WriteLine(matrix.ToLaTeX("vmatrix"));
-            //Console.WriteLine("========= SUBMATRIX =========");
-            //Console.WriteLine(matrix.Submatrix(1, 1, 3, 2));
-
+            ulong[] nums = TGeneration.Default.RandomArray<ulong>(10, 10, 100);
+            Console.WriteLine($"GCD of [{string.Join(',', nums)}] = {NumberTheory.GCD(nums)}");
+            Console.WriteLine($"LCM of [{string.Join(',', nums)}] = {NumberTheory.LCM(nums)}");
         }
     }
 }
---
+++ TMath/Numerics/AdvancedMath/NumberTheory/NumberTheory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+using static TMath.TFunctions;
+
+namespace TMath.Numerics.AdvancedMath.NumberTheory
+{
+	public static class NumberTheory
+	{ 
+		public static T GCD<T>(IEnumerable<T> source) where T : INumber<T>
+		{
+			return source.Aggregate(GCD);
+		}
+
+		public static T LCM<T>(IEnumerable<T> source) where T : INumber<T>
+		{
+			return source.Distinct().Aggregate((a, b) => a * b / GCD(a, b));
+		}
+
+		private static T GCD<T>(T a, T b) where T : INumber<T>
+		{
+			while(b != T.Zero)
+			{
+				(a, b) = (b, a % b);
+			}
+			return a;
+		}
+	}
+}
