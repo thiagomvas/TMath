9ee5ed95d12a7041c5a2d6797508dc0eecbe34df
Thiago <thiagomvas@gmail.com>
2024-02-28 18:31:37 -0300

    Add GeneratePermutations and GenerateCombinations

--- TMath/Numerics/TCombinatorics.cs
+++ TMath/Numerics/TCombinatorics.cs
@@ -8,8 +8,31 @@ namespace TMath.Numerics
 		public static T Permutations<T>(T n, T k) where T : INumber<T>, IBinaryInteger<T>
 			=> Factorial(n) / Factorial(n - k);
 
-		public static T Permutations<T>(IEnumerable<T> elements,T k ) where T : INumber<T>, IBinaryInteger<T>
+		public static T Permutations<T>(IEnumerable<T> elements, T k) where T : INumber<T>, IBinaryInteger<T>
 			=> Factorial(Helpers.Count<T, T>(elements)) / Factorial(Helpers.Count<T, T>(elements) - k);
+
+		public static IEnumerable<IEnumerable<T>> GeneratePermutations<T>(IEnumerable<T> elements, int k)
+		{
+			if (k == 0)
+			{
+				yield return new T[0];
+			}
+			else
+			{
+				int i = 0;
+				foreach (var element in elements)
+				{
+					var nextElements = elements.Where((e, index) => index != i);
+					foreach (var permutation in GeneratePermutations(nextElements, k - 1))
+					{
+						yield return new T[] { element }.Concat(permutation);
+					}
+					i++;
+				}
+			}
+		}
+
+
 
 		public static T Combinations<T>(T n, T k) where T : INumber<T>, IBinaryInteger<T>
 			=> Factorial(n) / (Factorial(k) * Factorial(n - k));
@@ -17,5 +40,27 @@ namespace TMath.Numerics
 		public static T Combinations<T>(IEnumerable<T> elements, T k) where T : INumber<T>, IBinaryInteger<T>
 			=> Factorial(Helpers.Count<T, T>(elements)) / (Factorial(k) * Factorial(Helpers.Count<T, T>(elements) - k));
 
+		public static IEnumerable<IEnumerable<T>> GenerateCombinations<T>(IEnumerable<T> elements, int k)
+		{
+			if (k == 0)
+			{
+				yield return new T[0];
+			}
+			else
+			{
+				int i = 0;
+				foreach (var element in elements)
+				{
+					var nextElements = elements.Where((e, index) => index > i);
+					foreach (var combination in GenerateCombinations(nextElements, k - 1))
+					{
+						yield return new T[] { element }.Concat(combination);
+					}
+					i++;
+				}
+			}
+		}
+
+		
 	}
 }
