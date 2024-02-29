f40500e8d420421e19f5646793ce18028bf665eb
Thiago <thiagomvas@gmail.com>
2024-02-29 10:44:04 -0300

    Add XML Doc comments

--- TMath/Numerics/TCombinatorics.cs
+++ TMath/Numerics/TCombinatorics.cs
@@ -5,11 +5,44 @@ namespace TMath.Numerics
 {
 	public static class TCombinatorics
 	{
+		/// <summary>
+		/// Calculates the number of permutations for selecting k elements from a set of n elements.
+		/// </summary>
+		/// <remarks>
+		/// Permutations represent the number of ways to arrange k elements from a set of n elements without replacement.
+		/// Requires numbers to implement <see cref="INumber{T}"/> and <see cref="IBinaryInteger{T}"/> interfaces.
+		/// </remarks>
+		/// <typeparam name="T">Type of numbers implementing <see cref="INumber{T}"/> and <see cref="IBinaryInteger{T}"/>.</typeparam>
+		/// <param name="n">The total number of elements in the set.</param>
+		/// <param name="k">The number of elements to select for each permutation.</param>
+		/// <returns>The number of permutations for selecting k elements from a set of n elements.</returns>
+
 		public static T Permutations<T>(T n, T k) where T : INumber<T>, IBinaryInteger<T>
 			=> Factorial(n) / Factorial(n - k);
 
-		public static T Permutations<T>(IEnumerable<T> elements, T k) where T : INumber<T>, IBinaryInteger<T>
-			=> Factorial(Helpers.Count<T, T>(elements)) / Factorial(Helpers.Count<T, T>(elements) - k);
+		/// <summary>
+		/// Calculates the number of permutations for selecting k elements from a collection of distinct elements.
+		/// </summary>
+		/// <remarks>
+		/// Permutations represent the number of ways to arrange k elements from a collection without replacement.
+		/// Requires numbers to implement <see cref="INumber{TTarget}"/> and <see cref="IBinaryInteger{TTarget}"/> interfaces.
+		/// </remarks>
+		/// <typeparam name="TSource">Type of elements in the collection.</typeparam>
+		/// <typeparam name="TTarget">Type of the count used for permutations, implementing <see cref="INumber{TTarget}"/> and <see cref="IBinaryInteger{TTarget}"/>.</typeparam>
+		/// <param name="elements">The collection of distinct elements.</param>
+		/// <param name="k">The number of elements to select for each permutation.</param>
+		/// <returns>The number of permutations for selecting k elements from the collection.</returns>
+
+		public static TTarget Permutations<TSource, TTarget>(IEnumerable<TSource> elements, TTarget k) where TTarget : INumber<TTarget>, IBinaryInteger<TTarget>
+			=> Factorial(Helpers.Count<TTarget, TSource>(elements)) / Factorial(Helpers.Count<TTarget, TSource>(elements) - k);
+
+		/// <summary>
+		/// Generates all permutations of k elements from a given collection.
+		/// </summary>
+		/// <typeparam name="T">Type of elements in the collection.</typeparam>
+		/// <param name="elements">The collection of elements to generate permutations from.</param>
+		/// <param name="k">The number of elements to select for each permutation.</param>
+		/// <returns>Enumerable collection of all permutations of k elements from the given collection.</returns>
 
 		public static IEnumerable<IEnumerable<T>> GeneratePermutations<T>(IEnumerable<T> elements, int k)
 		{
@@ -33,12 +66,44 @@ namespace TMath.Numerics
 		}
 
 
+		/// <summary>
+		/// Calculates the number of combinations for selecting k elements from a set of n elements.
+		/// </summary>
+		/// <remarks>
+		/// Combinations represent the number of ways to choose k elements from a set of n elements without considering the order.
+		/// Requires numbers to implement <see cref="INumber{T}"/> and <see cref="IBinaryInteger{T}"/> interfaces.
+		/// </remarks>
+		/// <typeparam name="T">Type of numbers implementing <see cref="INumber{T}"/> and <see cref="IBinaryInteger{T}"/>.</typeparam>
+		/// <param name="n">The total number of elements in the set.</param>
+		/// <param name="k">The number of elements to select for each combination.</param>
+		/// <returns>The number of combinations for selecting k elements from a set of n elements.</returns>
 
 		public static T Combinations<T>(T n, T k) where T : INumber<T>, IBinaryInteger<T>
 			=> Factorial(n) / (Factorial(k) * Factorial(n - k));
 
-		public static T Combinations<T>(IEnumerable<T> elements, T k) where T : INumber<T>, IBinaryInteger<T>
-			=> Factorial(Helpers.Count<T, T>(elements)) / (Factorial(k) * Factorial(Helpers.Count<T, T>(elements) - k));
+		/// <summary>
+		/// Calculates the number of combinations for selecting k elements from a collection of distinct elements.
+		/// </summary>
+		/// <remarks>
+		/// Combinations represent the number of ways to choose k elements from a collection without considering the order.
+		/// Requires numbers to implement <see cref="INumber{TTarget}"/> and <see cref="IBinaryInteger{TTarget}"/> interfaces.
+		/// </remarks>
+		/// <typeparam name="TSource">Type of elements in the collection.</typeparam>
+		/// <typeparam name="TTarget">Type of the count used for combinations, implementing <see cref="INumber{TTarget}"/> and <see cref="IBinaryInteger{TTarget}"/>.</typeparam>
+		/// <param name="elements">The collection of distinct elements.</param>
+		/// <param name="k">The number of elements to select for each combination.</param>
+		/// <returns>The number of combinations for selecting k elements from the collection.</returns>
+
+		public static TTarget Combinations<TSource, TTarget>(IEnumerable<TSource> elements, TTarget k) where TTarget : INumber<TTarget>, IBinaryInteger<TTarget>
+			=> Factorial(Helpers.Count<TTarget, TSource>(elements)) / (Factorial(k) * Factorial(Helpers.Count<TTarget, TSource>(elements) - k));
+
+		/// <summary>
+		/// Generates all combinations of k elements from a given collection.
+		/// </summary>
+		/// <typeparam name="T">Type of elements in the collection.</typeparam>
+		/// <param name="elements">The collection of elements to generate combinations from.</param>
+		/// <param name="k">The number of elements to select for each combination.</param>
+		/// <returns>Enumerable collection of all combinations of k elements from the given collection.</returns>
 
 		public static IEnumerable<IEnumerable<T>> GenerateCombinations<T>(IEnumerable<T> elements, int k)
 		{
