e59831a16596235125aa2f074a1fcc0e9c6a4317
Thiago <thiagomvas@gmail.com>
2024-03-03 19:50:49 -0300

    Add conversions

--- TMath/Types/Fraction.Conversions.cs
+++ TMath/Types/Fraction.Conversions.cs
@@ -12,42 +12,135 @@ namespace TMath.Types
 	{
 		public static implicit operator Fraction<T>(T value)
 		{
-			throw new NotImplementedException();
+			return new Fraction<T>(value);
 		}
 
 		public static explicit operator T(Fraction<T> value)
 		{
-			throw new NotImplementedException();
+			return value.Numerator / value.Denominator;
 		}
 
 		static bool INumberBase<Fraction<T>>.TryConvertFromChecked<TOther>(TOther value, out Fraction<T> result)
 		{
-			throw new NotImplementedException();
+			if(value is Fraction<T> f)
+			{
+				result = f;
+				return true;
+			}
+			try
+			{
+				var num = T.CreateChecked(value);
+				result = new Fraction<T>(num, T.One);
+				return true;
+			}
+			catch
+			{
+				result = Fraction<T>.Zero;
+				return false;
+			}
 		}
 
 		static bool INumberBase<Fraction<T>>.TryConvertFromSaturating<TOther>(TOther value, out Fraction<T> result)
 		{
-			throw new NotImplementedException();
+			if(value is Fraction<T> f)
+			{
+				result = f;
+				return true;
+			}
+			try
+			{
+				var num = T.CreateSaturating(value);
+				result = new Fraction<T>(num, T.One);
+				return true;
+			}
+			catch
+			{
+				result = Fraction<T>.Zero;
+				return false;
+			}
 		}
 
 		static bool INumberBase<Fraction<T>>.TryConvertFromTruncating<TOther>(TOther value, out Fraction<T> result)
 		{
-			throw new NotImplementedException();
+			if(value is Fraction<T> f)
+			{
+				result = f;
+				return true;
+			}
+			try
+			{
+				var num = T.CreateTruncating(value);
+				result = new Fraction<T>(num, T.One);
+				return true;
+			}
+			catch
+			{
+				result = Fraction<T>.Zero;
+				return false;
+			}
 		}
 
 		static bool INumberBase<Fraction<T>>.TryConvertToChecked<TOther>(Fraction<T> value, out TOther result)
 		{
-			throw new NotImplementedException();
+			if(value.Denominator == T.Zero)
+			{
+				result = default;
+				return false;
+			}
+
+			try
+			{
+				T val = (T)value;
+				result = TOther.CreateChecked(val);
+				return true;
+			}
+			catch
+			{
+				result = default;
+				return false;
+			}
 		}
 
 		static bool INumberBase<Fraction<T>>.TryConvertToSaturating<TOther>(Fraction<T> value, out TOther result)
 		{
-			throw new NotImplementedException();
+			if(value.Denominator == T.Zero)
+			{
+				result = default;
+				return false;
+			}
+
+			try
+			{
+				T val = (T)value;
+				result = TOther.CreateSaturating(val);
+				return true;
+			}
+			catch
+			{
+				result = default;
+				return false;
+			}
 		}
 
 		static bool INumberBase<Fraction<T>>.TryConvertToTruncating<TOther>(Fraction<T> value, out TOther result)
 		{
-			throw new NotImplementedException();
+			if(value.Denominator == T.Zero)
+			{
+				result = default;
+				return false;
+			}
+
+			try
+			{
+				T val = (T)value;
+				result = TOther.CreateTruncating(val);
+				return true;
+			}
+			catch
+			{
+				result = default;
+				return false;
+			}
 		}
 
 	}
